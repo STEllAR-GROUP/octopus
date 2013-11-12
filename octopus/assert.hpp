48b9a5dc44dca6d946596af322f81845a77176f5
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2013-11-12 13:53:28 -0600

    Add OCTOPUS_ALWAYS_ASSERT macros.

--- octopus/assert.hpp
+++ octopus/assert.hpp
@@ -12,10 +12,10 @@
 
 #include <octopus/config.hpp>
 
-#if OCTOPUS_ENABLE_VERIFICATION
-    #include <boost/current_function.hpp>
-    #include <boost/format.hpp>
+#include <boost/current_function.hpp>
+#include <boost/format.hpp>
 
+#if OCTOPUS_ENABLE_VERIFICATION
     #define OCTOPUS_ASSERT(expr) ((expr)                                \
       ? ((void)0)                                                       \
       : ::boost::assertion_failed                                       \
@@ -36,6 +36,22 @@
     #define OCTOPUS_ASSERT_MSG(expr, msg)
     #define OCTOPUS_ASSERT_FMT_MSG(expr, fmt, args)
 #endif
+
+#define OCTOPUS_ALWAYS_ASSERT(expr) ((expr)                         \
+  ? ((void)0)                                                       \
+  : ::boost::assertion_failed                                       \
+        (#expr, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
+
+#define OCTOPUS_ALWAYS_ASSERT_MSG(expr, msg) ((expr)                \
+  ? ((void)0)                                                       \
+  : ::boost::assertion_failed_msg                                   \
+        (#expr, msg, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
+
+#define OCTOPUS_ALWAYS_ASSERT_FMT_MSG(expr, fmt, args) ((expr)      \
+  ? ((void)0)                                                       \
+  : ::boost::assertion_failed_msg                                   \
+        (#expr, boost::str(boost::format(fmt) % args).c_str(),      \
+            BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
 
 #endif // OCTOPUS_C509AE65_1FE4_4FAD_96B5_BB75605EB37C
 
